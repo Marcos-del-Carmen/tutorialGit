b7158bc2755f1597c39d36b73615e059be771de8
Marcos Del Carmen <marcosdelcarmen768@gmail.com>
2023-05-16 17:35:19 -0600

    Agrege más archivos al proyecto

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function sumar(n1, n2) {
+    return n1 + n2;
+}
+
+function restar(n1, n2) {
+    return n1 - n2;
+}
+
+console.log(`La suma de los números es ${sumar(3, 14)}`);
+console.log(`La restar de los números es ${restar(15, 5)}`);
